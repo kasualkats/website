fed1460888d70a8111f131b4f1a075a60aaf67c3
theskycatyt <theskycatyt@gmail.com>
2024-06-23 13:29:01 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
 console.log("Hello");
 
+
+/*
 fetch('https://www.youtube.com/watch?v=6ruEbGhJZIg').then(function (response) {
 	if (response.ok) {
 		return response.text();
@@ -8,3 +10,18 @@ fetch('https://www.youtube.com/watch?v=6ruEbGhJZIg').then(function (response) {
 }).then(function (text) {
 	console.log(text);
 });
+*/
+
+fetch('https://www.youtube.com/watch?v=6ruEbGhJZIg', {
+    mode: 'no-cors'
+  })
+    .then(response => {
+      if (response.ok) {
+        console.log('Request successful:', response);
+      } else {
+        console.error('Request failed:', response);
+      }
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
